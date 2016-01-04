19c6f586c2d48ae8c49f54bb1d23741b3542e29e
KensoDev <avi@kensodev.com>
2016-01-04 08:45:07 -0800

    Fix typo in deploy script

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -5,5 +5,5 @@ set -e
 source secrets.sh
 # secrets.sh is intentionally excluded from the source code. It includes secret API keys and service ids
 bundle exec jekyll build
-cd _site && aws s4 sync . $S3_BUCKET --cache-control="public,max-age=3600"
+cd _site && aws s3 sync . $S3_BUCKET --cache-control="public,max-age=3600"
 curl -X POST -H "Fastly-Key: $FASTLY_KEY" https://api.fastly.com/service/$FASTLY_SERVICE_ID/purge_all
